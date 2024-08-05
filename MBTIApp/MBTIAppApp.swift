cff3f9823a04e40806b956186c5b87fe5fe6ae35
Teo <teccent@yandex.ru>
2024-08-05 20:37:50 +0300

    Initial Commit

---
+++ MBTIApp/MBTIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBTIAppApp.swift
+//  MBTIApp
+//
+//  Created by Теона Магай on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MBTIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
