6e83684f839d89e75644fcf3e9e453c727757966
Teo <teccent@yandex.ru>
2024-10-28 18:05:21 +0300

    добавлен navigation bar с тремя экранами (генерация типа, тест и общая информация), экраны представляют собой каркас без осмысленной информации. добавлено webview с ссылкой на сайт с тестом

---
+++ MBTIApp/GenerateView.swift
@@ -0,0 +1,18 @@
+//
+//  GenerateView.swift
+//  MBTIApp
+//
+//  Created by Теона Магай on 26.10.2024.
+//
+
+import SwiftUI
+
+struct GenerateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenerateView()
+}
---
+++ MBTIApp/TestLink.swift
@@ -0,0 +1,18 @@
+//
+//  TestLink.swift
+//  MBTIApp
+//
+//  Created by Теона Магай on 28.10.2024.
+//
+
+import SwiftUI
+
+struct TestLink: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestLink()
+}
